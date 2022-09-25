2e3251a56137465c52777bd12527563c90bc5bd8
finn1122 <mauroivaning@gmail.com>
2022-09-25 18:29:08 -0500

    primer comit

---
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,39 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class ComprasProductoPK {
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    @ManyToOne
+    @JoinColumn(name = "id_compra", insertable = false, updatable = false)
+    private Compra compra;
+
+    @ManyToOne
+    @JoinColumn(name = " id_prodcuto", insertable = false, updatable = false)
+    private Producto producto;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+}
